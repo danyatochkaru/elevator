48c6170ead4b93a017ebba53a796223e747d0869
danyatochkaru <annawer4589@gmail.com>
2021-12-26 04:47:01 +0300

    add to global router

--- back/router.js
+++ back/router.js
@@ -2,5 +2,12 @@ const { Router } = require('express')
 const router = Router()
 
 require('./routers/oreders.router')(router)
+require('./routers/cities.router')(router)
+require('./routers/clients.router')(router)
+require('./routers/incoming.router')(router)
+require('./routers/moving.router')(router)
+require('./routers/products.router')(router)
+require('./routers/storages.router')(router)
+require('./routers/users.router')(router)
 
 module.exports = router
