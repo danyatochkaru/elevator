5839df4b11e6b8734fdc5fdd8dafdc92c58051b3
ivan <ivan.safonov.09@mail.ru>
2021-12-26 15:29:18 +0300

    log_reg_ready

---
+++ front/script/script.js
@@ -0,0 +1,6 @@
+function visible(){
+    let auth =document.querySelector(".authorization");
+    auth.classList.toggle("reg_auth_visible");
+    let reg =document.querySelector(".registration");
+    reg.classList.toggle("reg_auth_visible");
+}
