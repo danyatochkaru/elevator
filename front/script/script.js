3dcbbb4de0952a38d8e83618fc7011f2f49e9d5c
ivan <ivan.safonov.09@mail.ru>
2021-12-28 04:30:53 +0300

    front90%(storage_moving)

--- front/script/script.js
+++ front/script/script.js
@@ -5,17 +5,7 @@ function visible(){
     let reg =document.querySelector(".registration");
     reg.classList.toggle("reg_auth_visible");
 }
-//Personal area visible
-function PA_drop(){
-    let drop = document.querySelector('.PA_drop');
-    if (drop.style.display!=="flex") {
-        drop.style.display="flex";
-        console.log(drop.style.display);
-    }else if (drop.style.display=="flex"){
-        drop.style.display="none"
-    }
-}
-//info_strage
+//info_orders
 const AUTH = {
     user_id:1
 }
@@ -82,25 +72,99 @@ const ORDERS = [
         ]
     }
 ]
+//info_storage
+const STORAGES={
+    storage_id:1,
+    name:"ЩЛЗ"
+}
+const STORAGE = [
+    {
+        incoming_id:1,
+        datetime: new Date('2021-12-11T13:13:13'),
+        products:[
+            {
+                product_id:1,
+                name:"Болт",
+                count:23,
+                price:23,
+                storage_id:2
+            }
+        ]
+    },
+    {
+        incoming_id:2,
+        datetime: new Date('2021-10-11T10:03:13'),
+        products:[
+            {
+                product_id:2,
+                name:"Гайка",
+                count:54,
+                price:18,
+                storage_id:1
+            },
+            {
+                product_id:3,
+                name:"Палка",
+                count:91,
+                price:10,
+                storage_id:1 
+            }
+        ]
+    },
+    {
+        incoming_id:3,
+        datetime: new Date('2021-09-11T04:13:13'),
+        products:[
+            {
+                product_id:1,
+                name:"Болт",
+                count:100,
+                price:23,
+                storage_id:3
+            }
+        ]
+    }
+]
 let html = ``;
-let options=ORDERS.map(item=>{
-    let time_op = {
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-      };
-    let products = item.products.map(product=>`${product.name} (${product.price})`);
-    html+=`<li><p>${item.order_id}</p>
-               <p>${products.join('<br>')}</p>
-               <p>${item.cost}</p>
-               <p>${CITIES.name}</p>
-               <p>${item.datetime.toLocaleString("ru", time_op)}</p>
-            </li>`
-})
-let orders_table = document.querySelector(".orders_table");
-orders_table.innerHTML=html;
-//storage_table
-// let storage_table = document.querySelector(".storage_table");
-// storage_table.innerHTML = html;
+if (document.location.href=="http://127.0.0.1:5500/front/page/orders.html") {
+    let options_order=ORDERS.map(item=>{
+        let time_op = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          };
+        let products = item.products.map(product=>`${product.name} (${product.price})`);
+        html+=`<li><p>${item.order_id}</p>
+                   <p>${products.join('<br>')}</p>
+                   <p>${item.cost}</p>
+                   <p>${CITIES.name}</p>
+                   <p>${item.datetime.toLocaleString("ru", time_op)}</p>
+                </li>`
+    })
+    let orders_table = document.querySelector(".orders_table");
+    orders_table.innerHTML=html;  
+}else if(document.location.href=="http://127.0.0.1:5500/front/page/storage.html"){
+    let options_storage=STORAGE.map(item=>{
+        let time_op = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          };
+        let products = item.products.map(product=>`${product.name} (${product.price})`);
+        let count = item.products.map(product=>`${product.count}`);
+        let product_id=item.products.map(product=>`${product.product_id}`)
+        html+=`<li><p>${product_id.join('<br>')}</p>
+                   <p>${products.join('<br>')}</p>
+                   <p>${count.join('<br>')}</p>
+                   <p>${STORAGES.name}</p>
+                   <p>${item.datetime.toLocaleString("ru", time_op)}</p>
+                </li>`
+    })
+    let storage_table = document.querySelector(".storage_table");
+    storage_table.innerHTML = html;
+}
+
